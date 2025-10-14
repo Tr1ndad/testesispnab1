6a92f64f30eb1218a1c846a96efd287822542b02
[dyad] <git@dyad.sh>
2025-10-14 18:32:48 -0300

    Updated src/pages/Cadastro.tsx

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -15,6 +15,7 @@ const cadastroSchema = z.object({
   rua: z.string().min(3, "Rua inválida"),
   numero: z.string().min(1, "Número inválido"),
   bairro: z.string().min(3, "Bairro inválido"),
+  complemento: z.string().min(3, "complemento inválido"),
   area_cultural: z.string().min(1, "Selecione uma área cultural"),
   outras_areas: z.array(z.string()),
   curriculo: z.string().min(10, "Currículo inválido"),
