6dadb18c5ddc4f17ae7d065a7fc488d9824d2a7f
[dyad] <git@dyad.sh>
2025-10-14 18:42:50 -0300

    Updated src/components/ui/resizable.tsx

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -4,31 +4,30 @@ import * as ResizablePrimitive from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
 
-const ResizablePanelGroup = React.forwardRef<
-  React.ElementRef<typeof ResizablePrimitive.PanelGroup>,
-  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelGroup>
->(({ className, ...props }, ref) => (
+const ResizablePanelGroup = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
   <ResizablePrimitive.PanelGroup
-    ref={ref}
     className={cn(
       "flex h-full w-full data-[panel-direction=vertical]:flex-col",
       className
     )}
     {...props}
   />
-));
-ResizablePanelGroup.displayName = ResizablePrimitive.PanelGroup.displayName;
+);
+ResizablePanelGroup.displayName = "ResizablePanelGroup";
 
 const ResizablePanel = ResizablePrimitive.Panel;
 
-const ResizableHandle = React.forwardRef<
-  React.ElementRef<typeof ResizablePrimitive.PanelResizeHandle>,
-  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelResizeHandle> & {
-    withHandle?: boolean;
-  }
->(({ className, withHandle = false, ...props }, ref) => (
+const ResizableHandle = ({
+  className,
+  withHandle = false,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+  withHandle?: boolean;
+}) => (
   <ResizablePrimitive.PanelResizeHandle
-    ref={ref as any}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 after:bg-border data-[panel-direction=vertical]:h-px data-[panel-direction=vertical]:w-full data-[panel-direction=vertical]:flex-col data-[panel-direction=vertical]:after:left-0 data-[panel-direction=vertical]:after:top-1/2 data-[panel-direction=vertical]:after:h-1 data-[panel-direction=vertical]:after:w-full data-[panel-direction=vertical]:after:-translate-y-1/2 data-[panel-direction=vertical]:after:translate-x-0 data-[resize-handle-state=hover]:bg-primary/80 data-[resize-handle-state=hover]:after:bg-primary/80 data-[resize-handle-state=drag]:bg-primary data-[resize-handle-state=drag]:after:bg-primary",
       className
@@ -41,7 +40,7 @@ const ResizableHandle = React.forwardRef<
       </div>
     )}
   </ResizablePrimitive.PanelResizeHandle>
-));
-ResizableHandle.displayName = ResizablePrimitive.PanelResizeHandle.displayName;
+);
+ResizableHandle.displayName = "ResizableHandle";
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
