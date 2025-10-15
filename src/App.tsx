84eab6208e2af975ad95ad3968a953690a1dadaf
[dyad] <git@dyad.sh>
2025-10-15 15:46:14 -0300

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ const AppContent = () => {
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryclient}>
+    <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <AuthProvider>
           <Toaster />
